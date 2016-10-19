27f75b5b92af06372284a31b66d0f9579a49144e
Dani Adkins <danirenaeadkins12@gmail.com>
2016-10-18 21:08:34 -0500

    fixed activate events function and event listener

--- activate.js
+++ activate.js
@@ -3,7 +3,7 @@ var CarLot = (function(oldIife){
     var currentSelection = "";
     var containerActivate = document.getElementsByClassName("car_card");
     var userInput = document.getElementById("input");
-    var currentDiv; /// william showed me this
+    var currentDiv;
 
       oldIife.activateEvents = function (){
         for (var i = 0; i < containerActivate.length; i++) {
@@ -11,41 +11,39 @@ var CarLot = (function(oldIife){
               console.log(e);
               for (var j = 0; j < containerActivate.length; j++) {
                 containerActivate[j].classList.remove("on-click");
+                userInput.value = "";
               }
               e.currentTarget.classList.add("on-click");
-              currentSelection = e.target;
+              currentSelection = e.currentTarget;
+              console.log(currentSelection);
               userInput.focus();
             });
-            console.log(currentSelection)
 
          userInput.addEventListener('keyup', function(go) {
           if (currentSelection) {
             var get = document.getElementById('description').innerHTML
-            // currentSelection.querySelector('.description').innerHTML = this.value;
-            get = go.currentTarget.children;
-
+            console.log(get);
+            currentSelection.querySelector('.description').innerHTML = this.value;
         }
     });
   }
 
 
-userInput.addEventListener('keyup', clearInput);
+    userInput.addEventListener('keyup', clearInput);
 
-function clearInput (e){
-  e.preventDefault();
-  if(e.keyCode == 13)
-  userInput.value = "";
-}
+    function clearInput (e){
+      e.preventDefault();
+      if(e.keyCode == 13)
+      userInput.value = "";
+      // userInput.blur();
     }
+  }
 
       return oldIife;
 
 })(CarLot || {});
 
-// the click event should set the current div to whatever you click - can get access to description here
-// you an set that to the currentDiv or something like this
-//- notes from Will
-//keyup event should edit that currentDiv description
+
 
 
 
